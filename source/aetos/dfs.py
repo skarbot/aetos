6959275d0331b8c57cae8df378671842c777d485
Kartik Sundarajan <kartiksundarajan@gmail.com>
2013-03-02 00:17:08 +0530

    Init dfs

---
+++ source/aetos/dfs.py
@@ -0,0 +1,8 @@
+'''Given a graph input with nodes as first row and connected edges 
+as second row perform a depth first search algorithm to compute
+the stongly connected component.
+'''
+
+def dfs(nodes, explored):
+    '''Given *nodes* '''
+    
